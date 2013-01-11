66861de53d6e0dd9cf11a63284eb48759547d724
Daniel Crispell <dan@visionsystemsinc.com>
2013-01-11 18:29:36 -0500

    added geometry_utils

---
+++ geometry_utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def fit_plane_3d(x_vals, y_vals, z_vals):
+
+  num_pts = len(x_vals)
+  P = np.zeros((4,num_pts))
+  P[0,:] = x_vals - x_vals.mean()
+  P[1,:] = y_vals - y_vals.mean()
+  P[2,:] = z_vals - z_vals.mean()
+
+  radius = np.sqrt((P[0:3,:] * P[0:3,:]).sum(0)).mean()
+  P = P / radius
+  P[3,:] = 1 
+  
+  A = np.dot(P,P.transpose())
+  
+  U,S,Vh = np.linalg.linalg.svd(A)
+  V = Vh.conj().transpose()
+  plane = V[:,-1]
+
+  # normal is unit vector
+  plane[0:3] = plane[0:3] / np.sqrt((plane[0:3]*plane[0:3]).sum())
+  # TODO: un-normalize!!
+
+  return plane
+
+
