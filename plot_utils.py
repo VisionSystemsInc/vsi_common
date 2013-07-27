543c6bba0648d2942f0fa12c1b6d6500bc90b3ad
Daniel Crispell <dan@visionsystemsinc.com>
2013-07-27 13:52:15 -0400

    added method for plotting multiple cameras

--- plot_utils.py
+++ plot_utils.py
@@ -1,7 +1,26 @@
+""" A set of utility functions related to plotting
+"""
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 def plot_box(x, y, width, height, *args, **kwargs):
-  xvec = [x, x+width, x+width, x, x]
-  yvec = [y, y, y+height, y+height, y]
-  plt.plot(xvec,yvec, *args, **kwargs)
+    """ plot a 2-d box """
+    xvec = [x, x+width, x+width, x, x]
+    yvec = [y, y, y+height, y+height, y]
+    plt.plot(xvec, yvec, *args, **kwargs)
+
+
+def plot_cameras(Ks, Rs, Ts, axis_len=1.0, plot_look=True):
+    """ plot a 3-d representation of a perspective camera """
+    centers = [-R.transpose().dot(T) for (R, T) in zip(Rs, Ts)]
+    centers_mat = np.array(centers).transpose()
+    # plot center of projection 
+    plt.plot(centers_mat[0, :], centers_mat[1, :], centers_mat[2, :],'ro')
+    if plot_look:
+        # plot a line in the direction of the principal axis
+        cam_zs = [R[2, :] for R in Rs]
+        for (center, cam_z) in zip(centers, cam_zs):
+            zax = np.vstack((center, center + axis_len*cam_z))
+            plt.plot(zax[:, 0], zax[:, 1], zax[:, 2],'g-')
 
