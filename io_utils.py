8149ffda519dda3e62559d0bda0b343409718210
Daniel Crispell <dan@visionsystemsinc.com>
2013-07-27 13:50:04 -0400

    some formatting changes, added read_bundler_file method

--- io_utils.py
+++ io_utils.py
@@ -1,9 +1,11 @@
-import string
+""" Utility functions related to input and output
+"""
 import numpy as np
 from PIL import Image
 import os
 
 def read_list(filename):
+    """ read a list of strings from file, one per line """
     try:
         fd = open(filename,'r')
     except IOError:
@@ -15,6 +17,7 @@ def read_list(filename):
     return lines
 
 def write_list(thelist, filename):
+    """ write each element to a seperate line in the file """
     try:
         fd = open(filename,'w')
     except IOError:
@@ -25,20 +28,22 @@ def write_list(thelist, filename):
     return
 
 def read_vector_float(filename):
-  lines = read_list(filename)
-  for line in lines:
-    elements_str = string.split(line)
+    """ read each float into a single vector """
     elements = []
-    for s in elements_str:
-      elements.append(float(s))
-  vec = np.array(elements)
-  return vec
+    lines = read_list(filename)
+    for line in lines:
+        elements_str = line.split()
+        for s in elements_str:
+            elements.append(float(s))
+    vec = np.array(elements)
+    return vec
 
 def read_vectors_float(filename):
+    """ read each line as a seperate vector of floats """
     lines = read_list(filename)
     vectors = []
     for line in lines:
-        elements_str = string.split(line)
+        elements_str = line.split()
         elements = []
         for s in elements_str:
             elements.append(float(s))
@@ -48,17 +53,19 @@ def read_vectors_float(filename):
     return vectors
 
 def write_vectors_float(vector_list, filename):
+    """ write each vector of floats to a seperate line """
     str_list = []
     for v in vector_list:
         v_str = ''
         for x in v:
-           v_str = v_str + str(x) + ' '
+            v_str = v_str + str(x) + ' '
         str_list.append(v_str)
-    write_list(str_list,filename)
+    write_list(str_list, filename)
     return str_list
     
             
 def imread(filename):
+    """ read the image to a 2-d numpy array """
     img = Image.open(filename)
     # workaround for 16 bit images
     if img.mode == 'I;16':
@@ -66,55 +73,87 @@ def imread(filename):
     return np.array(img)
 
 def imwrite(img, filename):
+    """ write the 2-d numpy array as an image """
     pilImg = Image.fromarray(img)
     if pilImg.mode == 'L':
-      pilImg.convert('I') # convert to 32 bit signed mode 
+        pilImg.convert('I') # convert to 32 bit signed mode 
     pilImg.save(filename)
     return
 
 def imwrite_byte(img, vmin, vmax, filename):
+    """ write the 2-d numpy array as an image, scale to byte range first """
     img_byte = np.uint8(np.zeros_like(img))
     img_norm = (img - vmin)/(vmax-vmin)
-    img_norm = img_norm.clip(0.0,1.0)
+    img_norm = img_norm.clip(0.0, 1.0)
     img_byte[:] = img_norm * 255
-    imwrite(img_byte,filename)
+    imwrite(img_byte, filename)
 
 # remove directory and extension from filename
 def filename_base(filename):
-    (path,filename_wext) = os.path.split(filename)
-    (base,ext) = os.path.splitext(filename_wext)
+    """ remove the directory and extension from the filename """
+    (path, filename_wext) = os.path.split(filename)
+    (base, ext) = os.path.splitext(filename_wext)
     return base
 
 def read_vector(vec_string):
-  elements_str = vec_string.split()
-  elements = []
-  for s in elements_str:
-    elements.append(float(s))
-  vec = np.array(elements)
-  return vec
-
-def read_matrix(row_strings):
-  rows = []
-  for line in row_strings:
-    elements_str = line.split()
+    """ read the individual floats from a string """
+    elements_str = vec_string.split()
     elements = []
     for s in elements_str:
-      elements.append(float(s))
-    if (len(elements) > 0):
-      vec = np.array(elements)
-      rows.append(vec)
-  M = np.array(rows)
-  return M
+        elements.append(float(s))
+    vec = np.array(elements)
+    return vec
+
+def read_matrix(row_strings):
+    """ read the individual matrix elements from a list of strings (one per row) """
+    rows = []
+    for line in row_strings:
+        elements_str = line.split()
+        elements = []
+        for s in elements_str:
+            elements.append(float(s))
+        if (len(elements) > 0):
+            vec = np.array(elements)
+            rows.append(vec)
+    M = np.array(rows)
+    return M
 
 def read_camera_KRT(filename):
-  lines = read_list(filename)
-  # remove any empty lines
-  lines = filter(None,lines)
-  K = read_matrix(lines[0:3])
-  R = read_matrix(lines[3:6])
-  T = read_vector(lines[6])
-  return K, R, T
+    """ read a KRT camera from text file """
+    lines = read_list(filename)
+    # remove any empty lines
+    lines = filter(None, lines)
+    K = read_matrix(lines[0:3])
+    R = read_matrix(lines[3:6])
+    T = read_vector(lines[6])
+    return K, R, T
 
-
- 
+def read_bundler_file(filename):
+    """ read an output file from the 'bundler' program.  Return the cameras and points """
+    lines = read_list(filename)
+    # first line is comment
+    # second line has number of cameras, number of pts
+    cam_pts_str = lines[1].split()
+    num_cams = int(cam_pts_str[0])
+    num_pts = int(cam_pts_str[1])
+    intrinsics = []
+    Rs = []
+    Ts = []
+    lines_per_cam = 5
+    for i in range(num_cams):
+        start_line = lines_per_cam * i + 2
+        intrinsic = read_vector(lines[start_line])
+        R = read_matrix(lines[start_line + 1: start_line + 4])
+        T = read_vector(lines[start_line + 4])
+        intrinsics.append(intrinsic)
+        Rs.append(R)
+        Ts.append(T)
+    pts = []
+    pts_start_line = lines_per_cam * num_cams + 2
+    lines_per_pt = 3
+    for i in range(num_pts):
+        start_line = pts_start_line + lines_per_pt*i
+        p = read_vector(lines[start_line])
+        pts.append(p)
+    return intrinsics, Rs, Ts, pts
 
