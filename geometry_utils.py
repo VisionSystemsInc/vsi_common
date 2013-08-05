09ed9d6edba79a758a44186c4a0a952397f6f305
Daniel Crispell <dancrispell@gmail.com>
2013-08-05 10:50:59 -0400

    formatting

--- geometry_utils.py
+++ geometry_utils.py
@@ -1,27 +1,29 @@
+""" Utility Functions for various geometric operations """
 import numpy as np
 
 def fit_plane_3d(x_vals, y_vals, z_vals):
+    """ fit a plane to a set of 3-d points """
 
-  num_pts = len(x_vals)
-  P = np.zeros((4,num_pts))
-  P[0,:] = x_vals - x_vals.mean()
-  P[1,:] = y_vals - y_vals.mean()
-  P[2,:] = z_vals - z_vals.mean()
+    num_pts = len(x_vals)
+    P = np.zeros((4, num_pts))
+    P[0, :] = x_vals - x_vals.mean()
+    P[1, :] = y_vals - y_vals.mean()
+    P[2, :] = z_vals - z_vals.mean()
 
-  radius = np.sqrt((P[0:3,:] * P[0:3,:]).sum(0)).mean()
-  P = P / radius
-  P[3,:] = 1 
-  
-  A = np.dot(P,P.transpose())
-  
-  U,S,Vh = np.linalg.linalg.svd(A)
-  V = Vh.conj().transpose()
-  plane = V[:,-1]
+    radius = np.sqrt((P[0:3, :] * P[0:3, :]).sum(0)).mean()
+    P = P / radius
+    P[3, :] = 1 
+    
+    A = np.dot(P, P.transpose())
+    
+    U, S, Vh = np.linalg.linalg.svd(A)
+    V = Vh.conj().transpose()
+    plane = V[:, -1]
 
-  # normal is unit vector
-  plane[0:3] = plane[0:3] / np.sqrt((plane[0:3]*plane[0:3]).sum())
-  # TODO: un-normalize!!
+    # normal is unit vector
+    plane[0:3] = plane[0:3] / np.sqrt((plane[0:3]*plane[0:3]).sum())
+    # TODO: un-normalize!!
 
-  return plane
+    return plane
 
 
