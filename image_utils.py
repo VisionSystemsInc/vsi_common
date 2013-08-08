fa01e17bb93ec21da17151c7fda04992fa758058
Daniel Crispell <dancrispell@gmail.com>
2013-08-08 16:21:53 -0400

    added ncc method

--- image_utils.py
+++ image_utils.py
@@ -1,10 +1,28 @@
 """ A collection of utility functions related to Image data """
 import numpy as np
+import PIL.Image as Image
+import PIL.ImageStat as ImageStat
 
 
 def rgb2gray(rgb):
-    """ convert an rgb image to grayscale """
-    gr = np.dot(rgb[..., :3], [0.299, 0.587, 0.144])
+    """ convert an rgb image stored as a numpy array to grayscale """
+    gr = np.dot(rgb[..., :3], [0.299, 0.587, 0.144]).astype(rgb.dtype)
     return gr
 
+def normalized_cross_correlation(img1, img2):
+    """ compute the normalized cross correlation of img1 and img2 """
+    i1 = np.array(img1,'float').ravel()
+    i2 = np.array(img2,'float').ravel()
+    i1 -= i1.mean()
+    norm1 = np.sqrt(np.sum(i1 * i1))
+    if norm1 > 0:
+        i1 /= norm1
+    i2 -= i2.mean()
+    norm2 = np.sqrt(np.sum(i2 * i2))
+    if norm2 > 0:
+        i2 /= norm2
+    return np.dot(i1, i2)
 
+
+
+    
