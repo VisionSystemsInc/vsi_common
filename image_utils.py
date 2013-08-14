8ff4eef81c0734e8be1546db5306cdfae1d58ae5
Daniel Crispell <dancrispell@gmail.com>
2013-08-14 12:19:14 -0400

    mutual_information works with byte images

--- image_utils.py
+++ image_utils.py
@@ -11,29 +11,28 @@ def rgb2gray(rgb):
 
 
 def mutual_information(img1, img2, min_val, max_val, nbins):
-    i1 = np.array(img1).ravel()
-    i2 = np.array(img2).ravel()
     """ compute mutual information of img1 and img2 """
-    counts = np.zeros((nbins,nbins))
+    i1 = np.array(img1).ravel().astype('float')
+    i2 = np.array(img2).ravel().astype('float')
     val_range = max_val - min_val
+
     # fill in counts
-    for (v0,v1) in izip(i1,i2):
-        b0 = int((v0 - min_val) / val_range * nbins)
-        b1 = int((v1 - min_val) / val_range * nbins)
-        counts[b0,b1] += 1
+    b1 = ((i1 - min_val) / val_range * nbins).astype('int')
+    b2 = ((i2 - min_val) / val_range * nbins).astype('int')
+    counts = np.zeros((nbins,nbins))
+    for (v1,v2) in izip(b1,b2):
+        counts[v1,v2] += 1
     total = float(len(i1))
     p0 = counts.sum(axis=0) / total
     p1 = counts.sum(axis=1) / total
     p01 = counts / total
-    mi = 0
-    for i in range(nbins):
-        for j in range(nbins):
-            if p01[i,j] == 0:
-                continue
-            if p0[i] == 0 or p1[j] == 0:
-                raise Exception
-            mi += p01[i,j] * np.log(p01[i,j] / (p0[i]*p1[j]))
-    return mi / np.log(nbins)
+    ind_prob = np.tile(p0.reshape(1,nbins),(nbins,1)) * np.tile(p1.reshape(nbins,1),(1,nbins))
+    # avoid divide by zero
+    zero_mask = (p01 == 0).astype('float')
+    p01_clean = p01 + zero_mask
+    ind_prob_clean = ind_prob + zero_mask
+    mi_matrix = p01 * np.log(p01_clean / ind_prob_clean)
+    return mi_matrix.sum() / np.log(nbins)
 
 
 def normalized_cross_correlation(img1, img2):
@@ -57,9 +56,10 @@ def sample_point(image, pt):
         return image.getpixel(tuple(pt))
     return None
 
+
 def sample_patch(image, corners, patch_size):
     """ return an Image of size patch_size, or None if the patch is outside image bounds """
-    if any ([c[0] < 0 or c[0] >= image.size[0] or c[1] < 0 or c[1] >= image.size[1] for c in corners]):
+    if any([c[0] < 0 or c[0] >= image.size[0] or c[1] < 0 or c[1] >= image.size[1] for c in corners]):
         return None
     corner_verts = (corners[0][0], corners[0][1],
                     corners[1][0], corners[1][1],
