d9743adaa07efa240416fb01639f9840213e00ec
Daniel Crispell <dancrispell@gmail.com>
2013-08-15 09:06:30 -0400

    add OrthoAnd3DPlot class

--- plot_utils.py
+++ plot_utils.py
@@ -24,45 +24,87 @@ def plot_cameras(Ks, Rs, Ts, axis_len=1.0, plot_look=True):
             zax = np.vstack((center, center + axis_len*cam_z))
             plt.plot(zax[:, 0], zax[:, 1], zax[:, 2],'g-')
 
+class OrthoAnd3DPlot:
+    """ 2x2 array of plots consisting of 3 ortho views, plus one 3-d view """
+    def __init__(self, fig, aligned_x_dim=0, aligned_y_dim=2, dim_labels=('X','Y','Z')):
+        self.fig = fig
+        self.ax_array = [[None,None],[None,None]]
+        self.ax_array[0][0] = fig.add_subplot(2,2,1)
+        self.ax_array[0][1] = fig.add_subplot(2,2,2)
+        self.ax_array[1][0] = fig.add_subplot(2,2,3)
+        self.ax_array[1][1] = fig.add_subplot(2,2,4,projection='3d')
+        self.aligned_x_dim = aligned_x_dim
+        self.aligned_y_dim = aligned_y_dim
+        self.dim_labels = dim_labels
+        # figure out the third axis
+        aligned_dims = (aligned_x_dim, aligned_y_dim)
+        self.extra_dim = 1
+        if not 0 in aligned_dims:
+            self.extra_dim = 0
+        elif not 2 in aligned_dims:
+            self.extra_dim = 2
 
-def init_orthos_and_3d():
-    """ initialize a 2x2 plot grid consisting of 3 ortho views, and one 3-d view """
-    fig = plt.figure()
-    ax_array = [[None,None],[None,None]]
+        self.label_axes()
 
-    ax_array[0][0] = fig.add_subplot(2,2,1)
-    ax_array[0][0].set_xlabel('X')
-    ax_array[0][0].set_ylabel('Z')
+    def label_axes(self):
+        """ label the axes using self.dim_labels """
+        self.ax_array[0][0].set_xlabel(self.dim_labels[self.aligned_x_dim])
+        self.ax_array[0][0].set_ylabel(self.dim_labels[self.aligned_y_dim])
 
-    ax_array[0][1] = fig.add_subplot(2,2,2)
-    ax_array[0][1].set_xlabel('Y')
-    ax_array[0][1].set_ylabel('Z')
+        self.ax_array[0][1].set_xlabel(self.dim_labels[self.extra_dim])
+        self.ax_array[0][1].set_ylabel(self.dim_labels[self.aligned_y_dim])
 
-    ax_array[1][0] = fig.add_subplot(2,2,3)
-    ax_array[1][0].set_xlabel('X')
-    ax_array[1][0].set_ylabel('Y')
+        self.ax_array[1][0].set_xlabel(self.dim_labels[self.aligned_x_dim])
+        self.ax_array[1][0].set_ylabel(self.dim_labels[self.extra_dim])
 
-    ax_array[1][1] = fig.add_subplot(2,2,4,projection='3d')
-    ax_array[1][1].view_init(60,-60)
-    ax_array[1][1].set_xlabel('X')
-    ax_array[1][1].set_ylabel('Y')
-    ax_array[1][1].set_zlabel('Z')
+        self.ax_array[1][1].set_xlabel(self.dim_labels[self.aligned_x_dim])
+        self.ax_array[1][1].set_ylabel(self.dim_labels[self.extra_dim])
+        self.ax_array[1][1].set_zlabel(self.dim_labels[self.aligned_y_dim])
 
-    return fig,ax_array
 
-def plot_orthos_and_3d(ax_array, items, *args, **kwargs):
-    """ add a set of 3-d elements to the plot """
-    if not (len(ax_array) == 2) and (len(ax_array[0]) == 2) and (len(ax_array[1]) == 2):
-        print('Error: expecting ax_array 2x2 list returned from init_orthos_and_3d')
+    def plot(self, items, *args, **kwargs):
+        """ add a set of 3-d elements to the plot. """
 
-    ax_array[0][0].plot(items[0,:], items[2,:], *args, **kwargs)
-    
-    ax_array[0][1].plot(items[1,:], items[2,:], *args, **kwargs)
+        self.ax_array[0][0].plot(items[self.aligned_x_dim,:],
+                            items[self.aligned_y_dim,:], *args, **kwargs)
+        
+        self.ax_array[0][1].plot(items[self.extra_dim,:],
+                            items[self.aligned_y_dim,:], *args, **kwargs)
 
-    ax_array[1][0].plot(items[0,:], items[1,:], *args, **kwargs)
+        self.ax_array[1][0].plot(items[self.aligned_x_dim,:],
+                            items[self.extra_dim,:], *args, **kwargs)
 
-    ax_array[1][1].plot(items[0,:], items[1,:], items[2,:], *args, **kwargs)
+        self.ax_array[1][1].plot(items[self.aligned_x_dim,:],
+                            items[self.extra_dim,:],
+                            items[self.aligned_y_dim,:], *args, **kwargs)
 
-    return
+    def setlim(self, dim, dmin, dmax):
+        """ set the axis limits on all plots for the given data dimension """
+        if dim == self.aligned_x_dim:
+            self.ax_array[0][0].set_xlim(dmin, dmax)
+            self.ax_array[1][0].set_xlim(dmin, dmax)
+            self.ax_array[1][1].set_xlim3d(dmin, dmax)
+        elif dim == self.aligned_y_dim:
+            self.ax_array[0][0].set_ylim(dmin, dmax)
+            self.ax_array[0][1].set_ylim(dmin, dmax)
+            self.ax_array[1][1].set_zlim3d(dmin, dmax)
+        else:
+            self.ax_array[0][1].set_xlim(dmin, dmax)
+            self.ax_array[1][0].set_ylim(dmin, dmax)
+            self.ax_array[1][1].set_ylim3d(dmin, dmax)
 
+    def invert_axis(self, dim):
+        """ invert the display of the given data dimension """
+        if dim == self.aligned_x_dim:
+            self.ax_array[0][0].invert_xaxis()
+            self.ax_array[1][0].invert_xaxis()
+            self.ax_array[1][1].invert_xaxis()
+        elif dim == self.aligned_y_dim:
+            self.ax_array[0][0].invert_yaxis()
+            self.ax_array[0][1].invert_yaxis()
+            self.ax_array[1][1].invert_zaxis()
+        else:
+            self.ax_array[0][1].invert_xaxis()
+            self.ax_array[1][0].invert_yaxis()
+            self.ax_array[1][1].invert_yaxis()
 
