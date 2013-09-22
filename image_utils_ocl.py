2fa26ebacf3145bd03340077cd653113056cf46e
Daniel Crispell <dancrispell@gmail.com>
2013-09-22 10:03:38 -0400

    added "batch" NCC scoring method.

--- image_utils_ocl.py
+++ image_utils_ocl.py
@@ -5,6 +5,43 @@ import pyopencl as cl
 import os
 
 
+def NCC_score_image(ocl_ctx, images, masks, window_radius):
+    """ compute a sliding NCC score based on a set of images with masks """
+
+    cl_queue = cl.CommandQueue(ocl_ctx)
+
+    num_images = len(images)
+    img_dims = images[0].size
+
+    # create large buffers of concatentated images and masks
+    image_stack = np.zeros((num_images*img_dims[1], img_dims[0]),np.float32)
+    mask_stack = np.zeros((num_images*img_dims[1], img_dims[0]),np.uint8)
+    for i in range(num_images):
+        image_stack[i*img_dims[1]:(i+1)*img_dims[1],:] = np.array(images[i]).astype(np.float32)
+        mask_stack[i*img_dims[1]:(i+1)*img_dims[1],:] = np.array(masks[i]).astype(np.uint8)
+
+    mf = cl.mem_flags
+    image_buff = cl.Buffer(ocl_ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=image_stack)
+    mask_buff = cl.Buffer(ocl_ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=mask_stack)
+
+    score_img = np.zeros((img_dims[1], img_dims[0]),np.float32)
+    output_buff = cl.Buffer(ocl_ctx, mf.WRITE_ONLY, score_img.nbytes)
+
+    cl_dir = os.path.dirname(__file__)
+    cl_filename = cl_dir + '/cl/NCC_score_multi.cl'
+    with open(cl_filename, 'r') as fd:
+        clstr = fd.read()
+
+    prg = cl.Program(ocl_ctx, clstr).build()
+    prg.NCC_score_multi(cl_queue, (img_dims[1], img_dims[0]), None,
+                        image_buff, mask_buff, output_buff, np.int32(num_images),
+                        np.int32(img_dims[0]), np.int32(img_dims[1]), np.int32(window_radius))
+
+    cl.enqueue_copy(cl_queue, score_img, output_buff)
+    cl_queue.finish()
+    return score_img, image_stack, mask_stack
+
+
 def sliding_NCC(ocl_ctx, img1, img2, window_radius):
     """ perform normalized cross-corellation on a window centered around every pixel """
 
