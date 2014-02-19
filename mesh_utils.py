e57d6a31bc28447ebe1c23011a4dea0de40bffa9
Daniel Crispell <dancrispell@gmail.com>
2014-02-19 18:31:07 -0500

    added some vtk wrappers

--- mesh_utils.py
+++ mesh_utils.py
@@ -1,6 +1,62 @@
 """ Utility functions related to mesh processing """
 import numpy as np
+import io_utils
 import vtk
+import glob
+
+
+def write_grid_vtk(image_stack_glob, output_filename, origin=(0,0,0), vox_len=1.0):
+    """ write out an image stack to a VTK "Structured Points" format """
+    image_fnames = glob.glob(image_stack_glob)
+    image_fnames.sort()
+    nk = len(image_fnames)
+    if nk == 0:
+        raise Exception('No images matching ' + image_stack_glob)
+
+    # read first image to get x,y dimensions
+    img0 = io_utils.imread(image_fnames[0])
+
+    ni = img0.shape[1]
+    nj = img0.shape[0]
+
+    fd = open(output_filename, 'w')
+    fd.write('# vtk DataFile Version 3.1\n')
+    fd.write('a voxel grid\n')
+    fd.write('ASCII\n')
+    fd.write('DATASET STRUCTURED_POINTS\n')
+    fd.write('DIMENSIONS %d %d %d\n' % (ni,nj,nk))
+    fd.write('ORIGIN %f %f %f\n' % (origin[0], origin[1], origin[2]))
+    fd.write('SPACING %f %f %f\n' % (vox_len, vox_len, vox_len))
+    fd.write('POINT_DATA %d\n' % (ni*nj*nk))
+    fd.write('SCALARS values float 1\n')
+    fd.write('LOOKUP_TABLE default\n')
+    for k in range(nk):
+        img = io_utils.imread(image_fnames[k])
+        for j in range(nj):
+            for i in range(ni):
+                fd.write('%f\n' % (float(img[j,i]) / 255.0))
+    fd.write('\n')
+    fd.close()
+
+
+def marching_cubes(grid_fname_vtk, mesh_filename_ply, value=0.5):
+    """ read in a grid in vtk format, run marching cubes, save the result as ply mesh """
+    reader = vtk.vtkStructuredPointsReader()
+    reader.SetFileName(grid_fname_vtk)
+    reader.Update()
+    points = reader.GetOutput()
+
+    mcubes = vtk.vtkMarchingCubes()
+    mcubes.SetInput(points)
+    mcubes.SetValue(0, value)
+    mcubes.Update()
+    mesh = mcubes.GetOutput()
+
+    writer = vtk.vtkPLYWriter()
+    writer.SetInput(mesh)
+    writer.SetFileName(mesh_filename_ply)
+    writer.Update()
+    writer.Write()
 
 
 def colorize_verts_ply(ply_in_filename, ply_out_filename, image, camera):
