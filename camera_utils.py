2c57c1a4c5706040866f3f5e15f065b9701a6257
Daniel Crispell <dancrispell@gmail.com>
2014-09-29 16:13:18 -0400

    move more pinhole stuff into projective

--- camera_utils.py
+++ camera_utils.py
@@ -3,10 +3,20 @@ import numpy as np
 import geometry_utils
 
 
-def construct_K(focal_len, image_size):
-    """ create calibration matrix K using the focal length and image.size
+def construct_K(image_size, focal_len=None, fov_degrees=None, fov_radians=None):
+    """ create calibration matrix K using the image size and focal length or field of view angle
     Assumes 0 skew and principal point at center of image
-    Note that image_size = (width, height) """
+    Note that image_size = (width, height)
+    Note that fov is assumed to be measured horizontally
+    """
+    if not np.sum([focal_len is not None, fov_degrees is not None, fov_radians is not None]) == 1:
+        raise Exception('Specify exactly one of [focal_len, fov_degrees, fov_radians]')
+
+    if fov_degrees is not None:
+        fov_radians = np.deg2rad(fov_degrees)
+    if fov_radians is not None:
+        focal_len = image_size[0] / (2.0 * np.tan(fov_radians/2.0))
+
     K = np.array([[focal_len, 0, image_size[0]/2.0], [0, focal_len, image_size[1]/2.0], [0, 0, 1]])
     return K
 
@@ -19,12 +29,16 @@ class ProjectiveCamera(object):
         if abs(self.P[2,3]) > 1e-6:
             self.P /= self.P[2,3]
 
+    def as_P(self, fd):
+        """ write the projection matrix to an ascii text file """
+        # write 3x4 projection matrix
+        for row in self.P:
+            fd.write('%f %f %f %f\n' % (row[0],row[1],row[2],row[3]))
+
     def saveas_P(self, filename):
         """ write the projection matrix to an ascii text file """
         with open(filename, 'w') as fd:
-            # write intrinsics K matrix
-            for row in self.P:
-                fd.write('%f %f %f %f\n' % (row[0],row[1],row[2],row[3]))
+            self.as_P(fd)
 
     def project_points(self, pts_3d):
         """ project pts_3d into image coordinates """
@@ -83,6 +97,52 @@ class ProjectiveCamera(object):
         """
         pts_3d = [self.backproject_point_plane(p, plane, return_homogeneous) for p in pts_2d]
         return pts_3d
+
+    def plane2image(self, plane_origin, plane_x, plane_y):
+        """ compute the transformation from points on a 3-d plane to image coordinates
+        """
+        plane_xlen = np.sqrt(np.dot(plane_x, plane_x))
+        plane_ylen = np.sqrt(np.dot(plane_y, plane_y))
+        plane_xu = plane_x / plane_xlen
+        plane_yu = plane_y / plane_ylen
+        plane_normal = np.cross(plane_xu, plane_yu)
+        plane2world_R = np.vstack((plane_xu, plane_yu, plane_normal)).transpose()
+        plane2world_T = plane_origin
+        plane_xy_scale = np.eye(4)
+        np.fill_diagonal(plane_xy_scale, (plane_xlen, plane_ylen, 1.0, 1.0))
+        plane2world_RT = np.vstack((np.hstack((plane2world_R, plane2world_T.reshape(3,1))),np.array((0,0,0,1))))
+        plane2world = np.dot(plane2world_RT, plane_xy_scale)
+
+        plane2img = np.dot(self.P, plane2world)
+        # we can remove the 3rd column since the "Z" coordinates of points on the plane are 0
+        plane2img3x3 = plane2img[:,(0,1,3)]
+        return plane2img3x3
+
+    def image2plane(self, plane_origin, plane_x, plane_y):
+        """ compute the transformation from image coordinates to points on a 3-d plane
+        """
+        #plane_xlen = np.sqrt(np.dot(plane_x, plane_x))
+        #plane_ylen = np.sqrt(np.dot(plane_y, plane_y))
+        #plane_xu = plane_x / plane_xlen
+        #plane_yu = plane_y / plane_ylen
+        #plane_normal = np.cross(plane_xu, plane_yu)
+        #world2plane_R = np.vstack((plane_xu, plane_yu, plane_normal))
+        #world2plane_T = -np.dot(world2plane_R, plane_origin)
+        #world2plane = np.vstack((np.hstack((world2plane_R, world2plane_T.reshape(3,1))),np.array((0,0,0,1))))
+
+        #cam2world_R = self.R.transpose()
+        #cam2world_T = -np.dot(cam2world_R, self.T)
+        #cam2world = np.vstack((np.hstack((cam2world_R, cam2world_T.reshape(3,1))),np.array((0,0,0,1))))
+        #Kinv4x3 = np.vstack((self.Kinv, np.zeros((1,3))))
+
+        #img2plane = np.dot(np.dot(world2plane, cam2world), Kinv4x3)
+        ## since the "Z" coordinate of the plane is 0, we can remove the 3rd row of the transform
+        ## make a deep copy so that matrix is contiguous
+        #img2plane3x3 = img2plane[(0,1,3),:].copy()
+
+        #return img2plane3x3
+        p2i = self.plane2image(plane_origin, plane_x, plane_y)
+        return np.linalg.inv(p2i)
 
 
 class PinholeCamera(ProjectiveCamera):
@@ -159,52 +219,6 @@ class PinholeCamera(ProjectiveCamera):
         pts = self.backproject_points((pt_2d,), (depth,))
         return pts[0]
 
-    def plane2image(self, plane_origin, plane_x, plane_y):
-        """ compute the transformation from points on a 3-d plane to image coordinates
-        """
-        plane_xlen = np.sqrt(np.dot(plane_x, plane_x))
-        plane_ylen = np.sqrt(np.dot(plane_y, plane_y))
-        plane_xu = plane_x / plane_xlen
-        plane_yu = plane_y / plane_ylen
-        plane_normal = np.cross(plane_xu, plane_yu)
-        plane2world_R = np.vstack((plane_xu, plane_yu, plane_normal)).transpose()
-        plane2world_T = plane_origin
-        plane_xy_scale = np.eye(4)
-        np.fill_diagonal(plane_xy_scale, (plane_xlen, plane_ylen, 1.0, 1.0))
-        plane2world_RT = np.vstack((np.hstack((plane2world_R, plane2world_T.reshape(3,1))),np.array((0,0,0,1))))
-        plane2world = np.dot(plane2world_RT, plane_xy_scale)
-
-        plane2img = np.dot(self.P, plane2world)
-        # we can remove the 3rd column since the "Z" coordinates of points on the plane are 0
-        plane2img3x3 = plane2img[:,(0,1,3)]
-        return plane2img3x3
-
-    def image2plane(self, plane_origin, plane_x, plane_y):
-        """ compute the transformation from image coordinates to points on a 3-d plane
-        """
-        #plane_xlen = np.sqrt(np.dot(plane_x, plane_x))
-        #plane_ylen = np.sqrt(np.dot(plane_y, plane_y))
-        #plane_xu = plane_x / plane_xlen
-        #plane_yu = plane_y / plane_ylen
-        #plane_normal = np.cross(plane_xu, plane_yu)
-        #world2plane_R = np.vstack((plane_xu, plane_yu, plane_normal))
-        #world2plane_T = -np.dot(world2plane_R, plane_origin)
-        #world2plane = np.vstack((np.hstack((world2plane_R, world2plane_T.reshape(3,1))),np.array((0,0,0,1))))
-
-        #cam2world_R = self.R.transpose()
-        #cam2world_T = -np.dot(cam2world_R, self.T)
-        #cam2world = np.vstack((np.hstack((cam2world_R, cam2world_T.reshape(3,1))),np.array((0,0,0,1))))
-        #Kinv4x3 = np.vstack((self.Kinv, np.zeros((1,3))))
-
-        #img2plane = np.dot(np.dot(world2plane, cam2world), Kinv4x3)
-        ## since the "Z" coordinate of the plane is 0, we can remove the 3rd row of the transform
-        ## make a deep copy so that matrix is contiguous
-        #img2plane3x3 = img2plane[(0,1,3),:].copy()
-
-        #return img2plane3x3
-        p2i = self.plane2image(plane_origin, plane_x, plane_y)
-        return np.linalg.inv(p2i)
-
     def rescale(self, scale_factor):
         """ Return a new camera corresponding to a resampled image
             Note: leaves calling object unmodified.
@@ -229,20 +243,24 @@ class PinholeCamera(ProjectiveCamera):
         """ compute and return the camera's x axis """
         return self.R[1,:]
 
-    def saveas_KRT(self, filename):
+    def as_KRT(self, fd):
         """ write the K,R,T matrices to an ascii text file """
-        with open(filename, 'w') as fd:
-            # write intrinsics K matrix
-            for row in self.K:
-                fd.write('%f %f %f\n' % (row[0],row[1],row[2]))
-            fd.write('\n')
-            # write rotation matrix
-            for row in self.R:
-                fd.write('%f %f %f\n' % (row[0],row[1],row[2]))
-            fd.write('\n')
-            # write translation vector
-            fd.write('%f %f %f\n' % (self.T[0],self.T[1],self.T[2]))
+        # write intrinsics K matrix
+        for row in self.K:
+            fd.write('%f %f %f\n' % (row[0],row[1],row[2]))
+        fd.write('\n')
+        # write rotation matrix
+        for row in self.R:
+            fd.write('%f %f %f\n' % (row[0],row[1],row[2]))
+        fd.write('\n')
+        # write translation vector
+        fd.write('%f %f %f\n' % (self.T[0],self.T[1],self.T[2]))
         return
+
+    def saveas_KRT(self, filename):
+        """ open the file and write K,R,T """
+        with open(filename, 'w') as fd:
+            self.as_KRT(fd)
 
 
 def triangulate_point(cameras, projections, return_homogeneous=True):
