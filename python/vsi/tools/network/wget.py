e36b966f5c373611c0016abcdade9830e5002f6c
Andy Neff <andrew.neff@visionsystemsinc.com>
2015-07-24 10:45:05 -0400

    Added some more dir_util capabilities

--- python/vsi/tools/network/wget.py
+++ python/vsi/tools/network/wget.py
@@ -4,49 +4,49 @@ import Cookie
 from StringIO import StringIO
 import zlib
 
-def authorizeBasic(user=None, password=None, realm=None, uri=None):
-  #This was needed. But not anymore
+def authorize_basic(user=None, password=None, realm=None, uri=None):
+  ''' Call before calling download '''
   if user and password and realm and uri:
     auth_handler = urllib2.HTTPBasicAuthHandler()
     auth_handler.add_password(realm=realm, uri=uri, user=user, passwd=password)
     opener = urllib2.build_opener(auth_handler)
     urllib2.install_opener(opener)
 
-def _makeCookieString(cookie):
-  simpleCookie = Cookie.SimpleCookie(cookie)
-  cookieString = []
-  for morselName in simpleCookie:
-    cookieString.append('%s=%s' % (morselName, simpleCookie[morselName].coded_value))
-  return '; '.join(cookieString)
+def _make_cookie_string(cookie):
+  simple_cookie = Cookie.SimpleCookie(cookie)
+  cookie_string = []
+  for morsel_name in simple_cookie:
+    cookie_string.append('%s=%s' % (morsel_name, simple_sookie[morsel_name].coded_value))
+  return '; '.join(cookie_string)
 
-def download(url, filename=None, chunkSize=2**20, cookie={}, disableSslCertVerify=False):
+def download(url, filename=None, chunk_size=2**20, cookie={}, disable_ssl_verify=False):
   #start simple, add feautres as time evolves
   #cookie - an optional dictionary
 
-  request = urllib2.Request(url);
-  request.add_header('Accept-encoding', 'gzip');
+  request = urllib2.Request(url)
+  request.add_header('Accept-encoding', 'gzip')
 
   if cookie:
-    request.add_header('Cookie', _makeCookieString(cookie))
+    request.add_header('Cookie', _make_cookie_string(cookie))
   
   context=None
-  if disableSslCertVerify and url.startswith('https://'):
+  if disable_ssl_verify and url.startswith('https://'):
     import ssl
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1) #disable cert checking
 
   try:
-    response = urllib2.urlopen(request, context=context);
+    response = urllib2.urlopen(request, context=context)
   except: #python pre 2.7
-    response = urllib2.urlopen(request);
+    response = urllib2.urlopen(request)
 
   if response.info().get('Content-Encoding') == 'gzip':
-    gzipped = True;
+    gzipped = True
     decompressor = zlib.decompressobj(16+zlib.MAX_WBITS)
     #Why 16+zlib.MAX_WBITS???
     #http://www.zlib.net/manual.html
     #Basically means gzip for zlib
   else:
-    gzipped = False;
+    gzipped = False
 
   if filename:
     output = open(filename, 'wb')
@@ -54,13 +54,13 @@ def download(url, filename=None, chunkSize=2**20, cookie={}, disableSslCertVerif
     from StringIO import StringIO
     output = StringIO()
     
-  buf = True;
+  buf = True
   while buf:
-    buf = response.read(chunkSize);
+    buf = response.read(chunk_size)
     if gzipped:
       output.write(decompressor.decompress(buf))
     else:
-      output.write(buf);
+      output.write(buf)
   
   if filename:
     output.close()
