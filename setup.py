f3c1b29847891b914325cc928ccba0a5b4b50e04
Andy Neff <andrew.neff@visionsystemsinc.com>
2016-01-20 10:35:33 -0500

    Fixed setup.py to include all modules

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 import os
 
-try:
-  # Use setuptools if available, for install_requires (among other things).
-  import setuptools
-  from setuptools import setup
-except ImportError:
-  setuptools = None
-  from distutils.core import setup
+from setuptools import find_packages, setup
 
 def read(fname):
   return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -22,7 +16,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'python'))
 setup(
     name='vsi_common',
     version='0.0.1',
-    packages=['vsi'],
+    packages=find_packages(where='python'),
     package_dir={'': 'python'},
     author = 'Andy Neff',
     author_email = 'andrew.neff@vsi-ri.com',
