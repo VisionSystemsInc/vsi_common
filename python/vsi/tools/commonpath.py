7a6738013c236882eea6a209a04ae7e4faa21c14
Scott Richardson <scott.richardson@visionsystemsinc.com>
2016-05-25 14:44:18 -0400

    didnt work with paths with different number of sub-directory components

--- python/vsi/tools/commonpath.py
+++ python/vsi/tools/commonpath.py
@@ -1,7 +1,21 @@
 import os.path as path
 
-def commonpath(uris): # use path.commonpath in python3.5
-  if isinstance(uris, basestring): # Python 3: isinstance(v, str)
-    uris = [uris]
-  return uris[0] if all(uris[0] == uri for uri in uris) \
-      else common_dirname([path.dirname(uri) for uri in uris])
+def commonpath(uris):
+  # use path.commonpath in python3.5
+  #
+  # >>> common_dirname(['/root/path/file.txt', '/root/file.txt'])
+  # '/root'
+  # >>> common_dirname(['root/path/file.txt', 'root/file.txt'])
+  # 'root'
+  
+  def subdirs(stems):
+    if isinstance(stems, basestring): # Python 3: isinstance(v, str)
+      stems = [stems]
+    
+    def isroot(p):
+      return path.dirname(p) == p
+    stems.append(path.dirname(stems[-1]))
+    return stems if path.dirname(stems[-1]) == '' else stems[:-1] if isroot(stems[-1]) else subdirs(stems)
+
+  for stem in subdirs(uris[0]):
+    if all(uri.startswith(stem) for uri in uris): return stem
