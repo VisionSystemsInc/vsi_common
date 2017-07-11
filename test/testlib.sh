e239746892d4c72e8ea9b97009fe937e0950e998
Andy Neff <andyneff@users.noreply.github.com>
2017-07-11 18:16:11 -0400

    Documentation and filename conventions

--- test/testlib.sh
+++ test/testlib.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
-# Usage: . testlib.sh
-# Simple shell command language test library.
+#!/usr/bin/env false
+
+#****J* vsi/testlib.sh
+# NAME
+#   testlib.sh - Simple shell command language test library
+# USAGE
+#   . testlib.sh
+# EXAMPLE
+#   Tests must follow the basic form:
 #
-# Tests must follow the basic form:
+#   source testlib.sh
 #
 #   begin_test "the thing"
 #   (
@@ -12,20 +18,23 @@
 #   )
 #   end_test
 #
-# When a test fails its stdout and stderr are shown.
-#
-# Note that tests must `set -e' within the subshell block or failed assertions
-# will not cause the test to fail and the result may be misreported.
-#
-# Copyright (c) 2011-13 by Ryan Tomayko <http://tomayko.com>
-# License: MIT
+#   When a test fails its stdout and stderr are shown.
+# NOTES
+#   Tests must 'set -e' within the subshell block or failed assertions will not
+#   cause the test to fail and the result may be misreported. While this is not
+#   required, most tests will have this on.
+# COPYRIGHT
+#   Original version: (c) 2011-13 by Ryan Tomayko <http://tomayko.com>
+#   License: MIT
+# AUTHOR
+#   Ryan Tomayko
+# MODIFICATION HISTORY
+#   Andy Neff - Added allowed_failures and teardown, removed PATH, added robodoc
+#***
 set -e
 
-# Put bin path on PATH
-PATH="$(cd $(dirname "$0")/.. && pwd)/bin:$PATH"
-
 # create a temporary work space
-TMPDIR="$(cd $(dirname "$0")/.. && pwd)"/tmp
+TMPDIR="$(cd "$(dirname "$0")"/.. && pwd)"/tmp
 TRASHDIR="$TMPDIR/$(basename "$0")-$$"
 
 # keep track of num tests and failures
@@ -33,9 +42,24 @@ tests=0
 failures=0
 allowed_failures=0
 
-# this runs at process exit
-atexit () {
-  rm -rf "$TRASHDIR"
+#****f* testlib.sh/atexit
+# NAME
+#   atexit - Function that runs at process exit
+# USAGE
+#   Automatically called on exit by trap.
+#
+#   Checks to see if teardown is defined, and calls it. teardown is typically a
+#   function, alias, or something that makes sense to call.
+# AUTHOR
+#   Ryan Tomayko
+# MODIFICATION HISTORY
+#   Andy Neff - Added teardown
+#***
+atexit () 
+{
+  if type -t teardown &>/dev/null; then
+    teardown
+  fi
 
   if [ $failures -gt 0 ]; then
     exit 1
@@ -49,9 +73,8 @@ trap "atexit" EXIT
 mkdir -p "$TRASHDIR"
 cd "$TRASHDIR"
 
-# Mark the beginning of a test. A subshell should immediately follow this
-# statement.
-function _begin_common_test () 
+# Common code for begin tests
+_begin_common_test () 
 {
   test_status=$?
   [ -n "$test_description" ] && end_test $test_status
@@ -69,20 +92,44 @@ function _begin_common_test ()
   set -x +e
 }
 
-function begin_test ()
+#****f* testlib.sh/begin_test
+# NAME
+#   begin_test - Beginning of test demarcation
+# USAGE
+#   Mark the beginning of a test. A subshell should immediately follow this
+#   statement.
+# AUTHOR
+#   Ryan Tomayko
+#***
+begin_test ()
 {
   allowed_failure=0
   _begin_common_test ${@+"${@}"}
 }
 
-function begin_fail_test()
+#****f* testlib.sh/begin_fail_test
+# NAME
+#   begin_fail_test - Beginning of failable test demarcation
+# USAGE
+#   Define the beginning of a test that is allowed to fail
+# AUTHOR
+#   Andy Neff
+#***
+begin_fail_test()
 {
   allowed_failure=1
   _begin_common_test ${@+"${@}"}
 }
 
-# Mark the end of a test.
-function end_test () 
+#****f* testlib.sh/begin_test
+# NAME
+#   begin_test - End of a test demarcation
+# USAGE
+#   Mark the end of a test.
+# AUTHOR
+#   Ryan Tomayko
+#***
+end_test () 
 {
   test_status="${1:-$?}"
   set +x -e
