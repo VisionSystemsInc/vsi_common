24a58b8bc519a3a9471d314dfb01e33fe0fb57ae
Andy Neff <andy@visionsystemsinc.com>
2019-01-16 16:52:48 -0500

    Added some string manip tool
    Signed-off-by: Andy Neff <andy@visionsystemsinc.com>

--- python/vsi/tools/python.py
+++ python/vsi/tools/python.py
@@ -620,9 +620,9 @@ def args_to_kwargs_unbound_easy(*args, **kwargs):
   '''
      Parameters
      ----------
-     *args
+     *args : args
           Variable length argument list.
-     **kwargs
+     **kwargs : args
           Arbitrary keyword arguments.
 
      Returns
@@ -656,7 +656,9 @@ def nested_update(d, *args, **kwargs):
   ----------
   d : dict
       The dict to be updated
-  *args, **kwargs :
+  *args : args
+      Same arguments as dict.update
+  **kwargs : args
       Same arguments as dict.update
   '''
   u = dict(*args, **kwargs)
