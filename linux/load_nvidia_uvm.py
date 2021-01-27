9467130fb94d4bb69429e697a082ee71313b2448
Andy Neff <andy@visionsystemsinc.com>
2021-01-27 15:26:50 -0500

    Improve load uvm script
    Signed-off-by: Andy Neff <andy@visionsystemsinc.com>

--- linux/load_nvidia_uvm.py
+++ linux/load_nvidia_uvm.py
@@ -21,32 +21,39 @@ import os
 from glob import glob
 import sys
 
-def main(args=[]):
+def find_cudart(search_dirs):
   cuda_runtimes=[]
 
+  for search_dir in search_dirs:
+    for root, dirs, files in os.walk(search_dir, followlinks=False):
+      cuda_runtimes.extend([os.path.join(root, rt) \
+                            for rt in files \
+                            if rt.startswith('libcudart.so')])
+      for cuda_runtime in cuda_runtimes:
+        try:
+          return c.cdll.LoadLibrary(cuda_runtime)
+        except OSError:
+          continue
+
+  return None
+
+def main(args=[]):
+
   if args:
-    cuda_runtimes=args
+    cudart = c.cdll.LoadLibrary(args[0])
   else:
     # Attempt to discover cuda runtime
     common_dirs = glob('/usr/local/cuda*') + glob('/usr/cuda*') + ['/usr'] \
                   + [os.path.dirname(__file__)]
+    cudart = find_cudart(common_dirs)
 
-    for common_dir in common_dirs:
-      for root, dirs, files in os.walk(common_dir, followlinks=False):
-        cuda_runtimes.extend([os.path.join(root, rt) \
-                            for rt in files \
-                            if rt.startswith('libcudart.so')])
-        if cuda_runtimes:
-          break
-      if cuda_runtimes:
-        break
-
-  if not cuda_runtimes:
+  if not cudart:
     print("Could not find cuda runtime. "
           "Please pass the full path of a cuda runtime as an argument")
+    print("Try running:")
+    print("  python " + __file__ + " path_to_libcudart.so")
     exit(1)
 
-  cudart = c.cdll.LoadLibrary(cuda_runtimes[0])
   cudart.cudaGetDeviceCount.argtypes = (c.POINTER(c.c_int),)
 
   gpu_count = c.c_int(-1)
