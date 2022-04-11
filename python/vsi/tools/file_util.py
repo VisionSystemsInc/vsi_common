632ecc80a0a3d135910ac5894410b308a6e31315
Andy Neff <andy@visionsystemsinc.com>
2022-04-11 15:25:54 -0400

    Add vsi.image
    - Also updated cuda_info
    - Added listing open ports to web_tools
    
    Signed-off-by: Andy Neff <andy@visionsystemsinc.com>

--- python/vsi/tools/file_util.py
+++ python/vsi/tools/file_util.py
@@ -20,3 +20,17 @@ def lncp(source, dest):
     os.symlink(source, dest)
   except:
     shutil.copyfile(source, dest)
+
+def file_or_temp(kwarg_key, filename):
+  '''
+  Decorator to replace a missing kwarg (missing, empty, or ``None``) that
+  defines a file with a filename in a valid temporary directory.
+
+  The temporary directory intended to contain the temporary file will be
+  created on entering the decorated function, and the directory & its contents
+  will be deleted on exiting the decorated function.
+
+  The decorated function is expected to create the file itself.
+  '''
+  kwarg_func = lambda temp_dir: os.path.join(temp_dir, filename)
+  return _path_or_temp(kwarg_key, kwarg_func)
