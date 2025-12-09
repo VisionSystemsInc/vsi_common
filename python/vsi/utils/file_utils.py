b1c5747717c1273c0792d2d8742aeaaddbad1361
Andy Neff <andy@visionsystemsinc.com>
2025-12-09 10:45:57 -0500

    Stop using unoffical API for rmtree
    - Vastly simplify rmtree by just using shutil.rmtree many times instead
      of trying to be super efficient and doing it all at once. Not worth
      the headache
    
    Signed-off-by: Andy Neff <andy@visionsystemsinc.com>

--- python/vsi/utils/file_utils.py
+++ python/vsi/utils/file_utils.py
@@ -11,101 +11,26 @@ import tempfile
 import logging
 logger = logging.getLogger(__name__)
 
-# This function was copied from cpython,
-# and changed to allow the base directory to be kept.
-# Source: https://github.com/python/cpython/blob/v3.6.15/Lib/shutil.py#L451
-def rmtree(path, ignore_errors=False, onerror=None, keep_base_dir=False):
+
+def rmtree(path, *args, keep_base_dir=False, **kwargs):
   """"
   shutil.rmtree with the ability to keep the directory at `path`. Necessary for situations
   when deleting the root directory would cause it to be unmounted.
 
   Parameters
   ----------
-  path : str
-      Path to the directory with contents to remove
-  ignore_errors : bool, optional
-      If True, exceptions will not be raised when an error is encountered. Default: False
-  onerror : Callable, optional
-      Function to be called when an error is encountered. Only used if ignore_errors is False.
-      Default: None
   keep_base_dir : bool, optional
       If True, the base directory will remain while its children are deleted. Default: False
+
+  The rest of ``shutil.rmtree``'s parameters should be passed along.
   """
-  if ignore_errors:
-    def onerror(*args):
-      pass
-  elif onerror is None:
-    def onerror(*args):
-      raise
-  if shutil._use_fd_functions:
-    # While the unsafe rmtree works fine on bytes, the fd based does not.
-    if isinstance(path, bytes):
-      path = os.fsdecode(path)
-    # Note: To guard against symlink races, we use the standard
-    # lstat()/open()/fstat() trick.
-    try:
-      orig_st = os.lstat(path)
-    except Exception:
-      onerror(os.lstat, path, sys.exc_info())
-      return
-    try:
-      fd = os.open(path, os.O_RDONLY)
-    except Exception:
-      onerror(os.lstat, path, sys.exc_info())
-      return
-    try:
-      if os.path.samestat(orig_st, os.fstat(fd)):
-        shutil._rmtree_safe_fd(fd, path, onerror)
-        if not keep_base_dir:
-          try:
-            os.rmdir(path)
-          except OSError:
-            onerror(os.rmdir, path, sys.exc_info())
-      else:
-        try:
-          # symlinks to directories are forbidden, see bug #1669
-          raise OSError("Cannot call rmtree on a symbolic link")
-        except OSError:
-          onerror(os.path.islink, path, sys.exc_info())
-    finally:
-      os.close(fd)
+  if keep_base_dir == False:
+    shutil.rmtree(path, *args, **kwargs)
   else:
-    return _rmtree_unsafe(path, onerror, keep_base_dir)
-
-
-# version vulnerable to race conditions
-def _rmtree_unsafe(path, onerror, keep_base_dir=False):
-  try:
-    if os.path.islink(path):
-      # symlinks to directories are forbidden, see bug #1669
-      raise OSError("Cannot call rmtree on a symbolic link")
-  except OSError:
-    onerror(os.path.islink, path, sys.exc_info())
-    # can't continue even if onerror hook returns
-    return
-  names = []
-  try:
     names = os.listdir(path)
-  except OSError:
-    onerror(os.listdir, path, sys.exc_info())
-  for name in names:
-    fullname = os.path.join(path, name)
-    try:
-      mode = os.lstat(fullname).st_mode
-    except OSError:
-      mode = 0
-    if stat.S_ISDIR(mode):
-      _rmtree_unsafe(fullname, onerror)
-    else:
-      try:
-        os.unlink(fullname)
-      except OSError:
-        onerror(os.unlink, fullname, sys.exc_info())
-  if not keep_base_dir:
-    try:
-      os.rmdir(path)
-    except OSError:
-      onerror(os.rmdir, path, sys.exc_info())
+    for name in names:
+      fullname = os.path.join(path, name)
+      shutil.rmtree(fullname, *args, **kwargs)
 
 
 def glob_files_with_extensions(directory, extensions, recursive=True):
