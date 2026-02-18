7896a46edd7914fb17084a59f7927c3ca5f3b7b5
Drew Gilliam <drew@visionsystemsinc.com>
2026-02-18 15:09:54 -0500

    test_glob.py use vsi.test.utils.TestCase

--- python/vsi/test/test_glob.py
+++ python/vsi/test/test_glob.py
@@ -1,24 +1,24 @@
-import unittest
-import tempfile
 import os
 import sys
-from distutils.dir_util import remove_tree
 from vsi.iglob import glob
-import posixpath
 from glob import glob as glob_orig
 
 from vsi.tools.dir_util import Chdir
+from vsi.test.utils import TestCase
 
-class GlobTest(unittest.TestCase):
+class GlobTest(TestCase):
   def setUp(self):
-    self.tempdir = tempfile.mkdtemp()
-    self.test1 = os.path.join(self.tempdir, 'test1')
-    self.test2 = os.path.join(self.tempdir, 'TeSt2')
+
+    super().setUp()
+
+    basedir = self.temp_dir.name
+    self.test1 = os.path.join(basedir, 'test1')
+    self.test2 = os.path.join(basedir, 'TeSt2')
 
     os.mkdir(self.test1)
     os.mkdir(self.test2)
-    open(os.path.join(self.tempdir, 'test3'), 'w').close()
-    open(os.path.join(self.tempdir, 'TESt4'), 'w').close()
+    open(os.path.join(basedir, 'test3'), 'w').close()
+    open(os.path.join(basedir, 'TESt4'), 'w').close()
 
     open(os.path.join(self.test1, 'file1.txt'), 'w').close()
     open(os.path.join(self.test1, 'FILE21.TXT'), 'w').close()
@@ -26,7 +26,7 @@ class GlobTest(unittest.TestCase):
     open(os.path.join(self.test2, 'file1.tXt'), 'w').close()
 
     if os.path.normcase('test1') != os.path.normcase('TEST1'):
-      self.test3 = os.path.join(self.tempdir, 'TEST1')
+      self.test3 = os.path.join(basedir, 'TEST1')
       os.mkdir(self.test3)
       open(os.path.join(self.test3, 'file1.txt'), 'w').close()
 
@@ -35,12 +35,12 @@ class GlobTest(unittest.TestCase):
     else:
       self.extra = 0
 
-    self.testData = [[os.path.join(self.tempdir, 'test*', '*'),    3,            4+self.extra, None],
-                     [os.path.join(self.tempdir, '*', '*.txt'),    1+self.extra, 4+self.extra, None],
-                     [os.path.join(self.tempdir, '?e*1', '*1*x*'), 1,            2+self.extra, None],
-                     [os.path.join(self.tempdir, '?e*', '*1*x*'),  1,            3+self.extra, None],
-                     [os.path.join(self.tempdir, 'test*'),         2,            4+self.extra, None],
-                     [os.path.join(self.tempdir, 'test*')+os.path.sep, 1,        2+self.extra, None],
+    self.testData = [[os.path.join(basedir, 'test*', '*'),         3,            4+self.extra, None],
+                     [os.path.join(basedir, '*', '*.txt'),         1+self.extra, 4+self.extra, None],
+                     [os.path.join(basedir, '?e*1', '*1*x*'),      1,            2+self.extra, None],
+                     [os.path.join(basedir, '?e*', '*1*x*'),       1,            3+self.extra, None],
+                     [os.path.join(basedir, 'test*'),              2,            4+self.extra, None],
+                     [os.path.join(basedir, 'test*')+os.path.sep,  1,        2+self.extra, None],
                      [self.test1.upper(),                          0,            1+self.extra, None],
                      [self.test1,                                  1,            1+self.extra, None],
                      [self.test1.upper()+os.path.sep,              0,            1+self.extra, None],
@@ -52,8 +52,8 @@ class GlobTest(unittest.TestCase):
                      [os.path.join(os.path.pardir, '*'),           5,            5,      self.test1],]
 
   def test_runAllTests(self):
-    for (pattern, result, resultI, testDir) in self.testData:
-      if testDir is not None:
+    for (pattern, result, resultI, basedir) in self.testData:
+      if basedir is not None:
         with Chdir(self.test1):
           self.run_test(pattern, result, resultI)
       else:
@@ -71,6 +71,3 @@ class GlobTest(unittest.TestCase):
     self.assertEqual(len(glob(pattern, False)), resultI)
     self.assertSortedEqual(glob(pattern), glob_orig(pattern))
 
-  def tearDown(self):
-    remove_tree(self.tempdir)
-    self.tempdir = None
